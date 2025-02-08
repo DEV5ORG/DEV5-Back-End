b2b5d7bd73635a27f1f019ae990c9aab2bf2268e
Steven Montoya Calderón <smontoyac@ucenfotec.ac.cr>
2025-02-08 11:42:24 -0600

    Changes in user model and adding dependencies for jwt

--- linkoapp/src/main/java/com/dev5/linkoapp/model/User.java
+++ linkoapp/src/main/java/com/dev5/linkoapp/model/User.java
@@ -17,22 +17,25 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
             @Column(name = "id")
-    Integer id;
+    private Integer id;
+
+    @Column(name = "username")
+    String username;
 
     @Column(name = "email")
-    String email;
+    private String email;
 
     @Column(name = "password")
-    String password;
+    private String password;
 
     @Column(name = "first_name")
-    String firstName;
+    private String firstName;
 
     @Column(name = "last_name")
-    String lastName;
+    private String lastName;
 
     @Enumerated(EnumType.STRING)
-    Role role;
+    private Role role;
 
     //User details methods
     @Override
