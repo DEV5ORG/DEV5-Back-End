ad35f307b103465ce23a1cbd4b14cc77b5db27ae
Steven Montoya Calderón <smontoyac@ucenfotec.ac.cr>
2025-02-08 10:45:28 -0600

    First commit, creating project

---
+++ linkoapp/src/main/java/com/dev5/linkoapp/LinkoappApplication.java
@@ -0,0 +1,13 @@
+package com.dev5.linkoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkoappApplication.class, args);
+    }
+
+}
---
+++ linkoapp/src/test/java/com/dev5/linkoapp/LinkoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev5.linkoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
