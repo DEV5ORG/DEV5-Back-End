297cb52393d11cc72d06850d609052649005a7ec
DouglasConejo <dconejoc@ucenfotec.ac.cr>
2025-02-10 21:27:37 -0600

    Add BD link

---
+++ dev5/src/main/java/com/dev5/dev5/Dev5Application.java
@@ -0,0 +1,13 @@
+package com.dev5.dev5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev5Application.class, args);
+	}
+
+}
---
+++ dev5/src/test/java/com/dev5/dev5/Dev5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev5.dev5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
